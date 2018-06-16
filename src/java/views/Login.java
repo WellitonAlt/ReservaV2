e40bc98859bd740f4baaf4957d94c9c48336bfad
Welliton Altimari <etoalt@hotmail.com>
2018-06-16 18:45:07 -0300

    Login

--- src/java/views/Login.java
+++ src/java/views/Login.java
@@ -2,11 +2,12 @@
 package views;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.naming.NamingException;
 import reserva.Hotel;
-import reserva.Login;
 import reserva.Site;
 import reserva.dao.HotelDAO;
 import reserva.dao.SiteDAO;
@@ -15,9 +16,15 @@ import reserva.dao.SiteDAO;
 @RequestScoped
 public class Login implements Serializable {
     
-    private String usuario;
-    private String senha;
-    private String tipo;
+    String usuario;
+    String senha;
+    String tipo;   
+    Hotel hotel;
+    Site site;
+    
+    @Inject 
+    HotelDAO hotelDAO;
+    SiteDAO siteDAO;
 
     public String getUsuario() { return usuario; }
 
@@ -30,17 +37,54 @@ public class Login implements Serializable {
     public String getTipo() { return tipo; }
 
     public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) { this.hotel = hotel; }
+
+    public Site getSite() { return site; }
+
+    public void setSite(Site site) { this.site = site; }
     
-    Login login = new Login();
-    Hotel hotel;
-    Site site;
-    
-    @Inject 
-    HotelDAO hotelDAO;
-    SiteDAO siteDAO;
-    
-    
-    
-    
-  
+    public String fazLogin(){        
+        System.out.println(usuario);
+        System.out.println(senha);
+        System.out.println(tipo);
+        
+        
+        try{
+            switch (tipo) {
+                case "adm":
+                    if(usuario.equals("root") && senha.equals("root")){
+                        //Adm
+                    }else{
+                        //Erro
+                    }    break;
+                case "hotel":
+                    hotel = hotelDAO.loginHotel(usuario, senha);
+                    if(hotel != null){
+                        //Hotel
+                    }
+                    else{
+                        //Erro
+                    }    break;
+                case "site":
+                    site = siteDAO.loginSite(usuario, senha);
+                    if(site != null){
+                        //Site
+                    }
+                    else{
+                        //Erro
+                    }    break;
+                default:
+                    //Nenhum Selecionado
+            }
+        }catch (NullPointerException | SQLException | NamingException ex) {
+            //Erro
+        }
+        return "login";
+    }  
+   
 }
